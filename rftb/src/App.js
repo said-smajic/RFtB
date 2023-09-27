39f2f9f67b8891477ba3876aeeac3d578659d954
Said Smajić <83903364+said-smajic@users.noreply.github.com>
2023-09-27 20:31:23 +0200

    s3-13

--- rftb/src/App.js
+++ rftb/src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <Header text />
+      <Header />
       <div className="container">
         <h1>MyApp</h1>
       </div>
